c20d4877db28962e0ad4ffcaa23aac9ee69601b4
Souvik Sarkar <souvik.sarkar@iemlabs.com>
2023-06-29 05:34:56 -0700

    General Layout is Done.

---
+++ app/_layout.js
@@ -0,0 +1,34 @@
+import { View, Image, StatusBar, StyleSheet } from "react-native";
+import { Stack } from "expo-router";
+
+import Footer from "../components/Footer";
+
+export default () => {
+    return (<>
+        <Stack screenOptions={{
+            statusBarColor: 'black',
+            header: () => (<View style={styles.header}>
+                <Image
+                    source={require('../public/logo-smart.png')}
+                    style={styles.logo}
+                />
+            </View>)
+        }}
+        />
+        <Footer/>
+    </>)
+}
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        backgroundColor: 'crimson',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: StatusBar.currentHeight
+    },
+    logo: {
+        backgroundColor: 'crimson'
+    }
+})
